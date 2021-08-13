a04853a584c2f8399111e262336d0d91e154bfca
zahid hasan <hzahid2999@gmail.com>
2021-08-14 00:22:06 +0600

    added initial app layout with column card and input field

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Column } from "./component/Column";
+import { Card } from "./component/Card";
+import { AddNewItem } from "./component/AddNewItem";
+import { AppContainer } from "./styles";
+
+export const App = () => {
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
+}
